fd077513cbb91d7b93e6f9f00a4e7b5395a9e795
UrbsKali <urbain.lantres@tutanota.com>
2024-11-15 20:01:35 +0000

    Fix Card color

--- src/lib/components/share/Card.svelte
+++ src/lib/components/share/Card.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div
-	class="rounded-xl border-[3.5px] border-light-blue dark:border-dark-light-blue p-4 w-[450px] flex flex-col gap-2 min-w-96"
+	class="rounded-xl border-[3.5px] border-dark-light-blue p-4 w-[450px] flex flex-col gap-2 min-w-96"
 >
 	<div class="flex justify-between">
 		<h1 class="text-2xl font-bold">{title}</h1>
@@ -14,7 +14,7 @@
 	<div class="flex gap-5">
 		<div class="max-w-[50%]">
 			<p class="mb-2 text-sm text-justify text-blue-gray dark:text-dark-blue-gray">{description}</p>
-			<a href={link} class=" text-light-blue dark:text-dark-light-blue">En savoir plus...</a>
+			<a href={link} class=" text-blue-peps dark:text-dark-light-blue">En savoir plus...</a>
 		</div>
 		<div id="img-mask" class="h-28">
 			<img src={image} alt={title} class="h-28 aspect-auto" />
