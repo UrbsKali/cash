6120b6f8c166a39700a3a7e169a82aff1d5c8f95
UrbsKali <urbain.lantres@tutanota.com>
2024-12-19 15:31:07 +0100

    fix build

--- svelte.config.js
+++ svelte.config.js
@@ -24,6 +24,7 @@ const config = {
 				// '/admin/projects/6',
 				// '/admin/projects/8',
 				'/admin/orders',
+				'/admin/treso',
 				'/admin/orders/new',
 				'/admin/users/',
 				'/admin/wip',
