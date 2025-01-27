ced37f6a0b8308e3689468d2e564cf0277601de7
gaelAphack <gael.le_mouel@edu.devinci.fr>
2025-01-27 22:17:36 +0100

    Begin to play with threlte

---
+++ src/lib/components/3d/Scene.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { T, useTask } from '@threlte/core';
+
+	let rotation = 0;
+	useTask((delta) => {
+		rotation += delta;
+	});
+</script>
+
+<T.PerspectiveCamera
+	makeDefault
+	position={[2, 3, 10]}
+	oncreate={(ref) => {
+		ref.lookAt(0, 1, 0);
+	}}
+/>
+
+<T.DirectionalLight position={[0, 10, 10]} castShadow />
+
+<T.Mesh position.y={1} rotation.y={rotation} castShadow>
+	<T.BoxGeometry args={[1, 1, 1]} />
+	<T.MeshStandardMaterial color="hotpink" />
+</T.Mesh>
+
+<T.Mesh rotation.x={-Math.PI / 2} receiveShadow>
+	<T.CircleGeometry args={[4, 40]} />
+	<T.MeshStandardMaterial color="white" />
+</T.Mesh>
---
+++ src/routes/3d/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { Canvas } from '@threlte/core';
+	import Scene from '../../lib/components/3d/Scene.svelte';
+</script>
+
+<Canvas>
+	<Scene />
+</Canvas>
