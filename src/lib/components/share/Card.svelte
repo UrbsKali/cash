0e533afbef559dbad3dc9b5ec17899ba1ca657d7
UrbsKali <urbain.lantres@tutanota.com>
2025-02-12 19:53:19 +0100

    Update card

--- src/lib/components/share/Card.svelte
+++ src/lib/components/share/Card.svelte
@@ -5,19 +5,17 @@
 	export let link = '';
 </script>
 
-<div
-	class="rounded-xl border-[3.5px] border-dark-light-blue p-4 w-[450px] flex flex-col gap-2 min-w-96"
->
+<div class="rounded-xl border-[3.5px] border-dark-light-blue p-4 min-w-[480px] flex flex-col gap-2">
 	<div class="flex justify-between">
 		<h1 class="text-2xl font-bold">{title}</h1>
 	</div>
-	<div class="flex gap-5">
-		<div class="max-w-[50%]">
+	<div class="flex gap-10">
+		<div class="max-w-[45%]">
 			<p class="mb-2 text-sm text-justify text-blue-gray dark:text-dark-blue-gray">{description}</p>
 			<a href={link} class=" text-blue-peps dark:text-dark-light-blue">En savoir plus...</a>
 		</div>
-		<div id="img-mask" class="h-28">
-			<img src={image} alt={title} class="h-28 aspect-auto" />
+		<div id="img-mask" class="h-32">
+			<img src={image} alt={title} class="h-32 aspect-16/9" />
 		</div>
 	</div>
 </div>
