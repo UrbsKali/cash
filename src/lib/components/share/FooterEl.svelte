4f846e86bc40997d3d6346b7e7a712294e71d27c
UrbsKali <urbain.lantres@tutanota.com>
2025-03-05 00:26:45 +0100

    Footer test

---
+++ src/lib/components/share/FooterEl.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let items = [{ text: '', href: '' }];
+	export let title = '';
+</script>
+
+<div
+	class="flex flex-col items-start content-between justify-between order-3 h-full text-left md:m-0"
+>
+	<p class="pb-1 font-bold uppercase dark:text-white">{title}</p>
+	<div class="flex flex-col items-start">
+		{#each items as item}
+			<a href={item.href} class="text-dark-blue-gray"> {@html item.text} </a>
+		{/each}
+	</div>
+</div>
