e38a2e1f37be0088b855f4c5bf3339a39776f8af
UrbsKali <urbain.lantres@tutanota.com>
2025-08-25 21:05:19 +0200

    noindex on auth page

---
+++ src/routes/auth/+layout.svelte
@@ -0,0 +1,31 @@
+<svelte:head>
+	<title>DaVinciBot - Authentification</title>
+	<meta name="description" content="Page d'authentification de DaVinciBot." />
+	<meta
+		name="keywords"
+		content="DaVinciBot, association, robot, robotique, étudiant, esilv, authentification"
+	/>
+	<meta name="author" content="DaVinciBot" />
+	<meta name="robots" content="noindex, nofollow" />
+
+	<!-- meta og -->
+	<meta property="og:title" content="DaVinciBot - Authentification" />
+	<meta property="og:description" content="Page d'authentification de DaVinciBot." />
+	<meta property="og:image" content="/dvb_og_img.png" />
+	<meta property="og:url" content="https://davincibot.fr/auth" />
+	<meta property="og:type" content="website" />
+	<meta property="og:site_name" content="DaVinciBot" />
+	<meta property="og:locale" content="fr_FR" />
+
+	<!-- meta twitter -->
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:site" content="@DaVinciBot" />
+	<meta name="twitter:creator" content="@DaVinciBot" />
+	<meta name="twitter:title" content="DaVinciBot - Authentification" />
+	<meta name="twitter:description" content="Page d'authentification de DaVinciBot." />
+	<meta name="twitter:image" content="/dvb_og_img.png" />
+	<meta name="twitter:image:alt" content="DaVinciBot - Authentification" />
+	<meta name="twitter:url" content="https://davincibot.fr/auth" />
+	<meta name="twitter:domain" content="davincibot.fr" />
+	<meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
